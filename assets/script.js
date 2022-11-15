fc5d8de96bae6660b757b506d0952ad03ce43e55
damiandlg <damian.delagarzaf@gmail.com>
2022-11-15 00:47:31 -0600

    java has funcionality

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,21 @@
 const form = document.querySelector(".top-banner form");
 const apiKey = "d98d785e9ed33288cf7b14ba756e798f";
 const inputVal = input.value;
- 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`)
+
+
+  function weatherFor() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`)
 .then(response => response.json())
   .then(data => {
     var nameValue = data['city']['name'] + ', '+ data['city']['country'] + '<br>' + data['cnt'];
+    var dayOne = data['list'][0]['dt_txt'] + '<br>' + 'Temp:' + data['list'][0]['main']['temp']  + '&#176;' + '<br>' + 'Humidity:' + data['list'][0]['main']['humidity'] + '%';
   })
   .catch(() => {
     msg.textContent = "City invalid, try again! 😩";
   });
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const inputVal = input.value;
-});
+  cityName.innerHTML = nameValue;
+  dayOneTemp.innerHTML = dayOne;
+  }
+
+  button.addEventListener('click', weatherFor)
