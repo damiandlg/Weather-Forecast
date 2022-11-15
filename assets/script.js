d79c3f45a72b4cd1be04b3ce1ca4c325859c8e3f
damiandlg <damian.delagarzaf@gmail.com>
2022-11-15 00:25:43 -0600

    added fetch method

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,16 @@
 const form = document.querySelector(".top-banner form");
 const apiKey = "d98d785e9ed33288cf7b14ba756e798f";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 const inputVal = input.value;
  
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`)
+.then(response => response.json())
+  .then(data => {
+    var nameValue = data['city']['name'] + ', '+ data['city']['country'] + '<br>' + data['cnt'];
+  })
+  .catch(() => {
+    msg.textContent = "City invalid, try again! 😩";
+  });
+
 form.addEventListener("submit", e => {
   e.preventDefault();
   const inputVal = input.value;
