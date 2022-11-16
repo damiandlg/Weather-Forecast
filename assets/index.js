e88c97704f955d02a037f38948442d7615892921
damiandlg <damian.delagarzaf@gmail.com>
2022-11-15 19:46:25 -0600

    lean code and css

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,12 @@
 //Function
 
-function getForecast(cityName, apiKey, success, error) {
+ async function getForecast(cityName, apiKey) {
   const path = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
   
-  const p1 = fetch(path)
-  const p2 = p1.then((res) => {
-      return res.json()
-    })
-    
-   return p2
+  const res = await fetch(path)
+  const json =  await res.json()
+          
+   return json
     
     
 }
