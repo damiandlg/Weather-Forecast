544b47de8da35a8d592a93d7534d8b6c781c4ecd
damiandlg <damian.delagarzaf@gmail.com>
2022-11-15 20:23:22 -0600

    MVP done

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,23 @@
 //Function
 
- async function getForecast(cityName, apiKey) {
-  const path = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-  
-  const res = await fetch(path)
-  const json =  await res.json()
-          
-   return json
-    
-    
-}
+ class Forecast {
+    constructor(apiKey, units = 'metric') {
+      this.apiKey = apiKey
+      this.units = units
+    }
+    async getForecast(cityName) {
+      const{ apiKey, units} = this
+      const path = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`
+      
+      const res = await fetch(path)
+      const json =  await res.json()
+              
+       return json
+        
+        
+    }
+ }
+
+
 
 
